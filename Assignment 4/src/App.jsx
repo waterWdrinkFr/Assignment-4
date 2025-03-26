5b0447e32343f0ec9c053e27bd9b80cc40af1663
JustinSu <Justin.su348@gmail.com>
2025-03-26 19:16:13 +0000

    changed class name

--- Assignment 4/src/App.jsx
+++ Assignment 4/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="calculators">
+    <div class="calculators">
       <Heron />
       <Ambig />
     </div>
