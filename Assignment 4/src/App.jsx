65a78c64c59dcac01aa7246aa57c7b29b81ff619
JustinSu <Justin.su348@gmail.com>
2025-03-31 00:24:19 -0400

    removed unnecessary useState from App.jsx

--- Assignment 4/src/App.jsx
+++ Assignment 4/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
-import { useState } from "react";
 import Heron from './components/heron.jsx';
 import Ambig from './components/ambig.jsx';
 import Newton from './components/newton.jsx';
 import Poly from './components/poly.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="calculators">
       <Heron />
